acd76b8b6569a01a5f041b3e419d3b4d01113cfa
Yichi Zhang <zhang-yi-chi@hotmail.com>
2015-02-01 19:43:25 +1100

    First commit; rewriting "- (void)presentPointingAtView:(UIView *)targetView inView:(UIView *)containerView animated:(BOOL)animated"

---
+++ CMPopTipView_Swift/CMPopTipView.swift
@@ -0,0 +1,83 @@
+//
+//  CMPopTipView.swift
+//  CMPopTipViewDemo
+//
+//  Created by Yichi on 1/02/2015.
+//  Copyright (c) 2015 Chris Miles. All rights reserved.
+//
+
+import Foundation
+
+extension CMPopTipView {
+    func t_presentPointingAtView(targetView:UIView, inView containerView:UIView, animated:Bool) {
+    
+        if targetObject == nil {
+            targetObject = targetView
+        }
+        
+        // If we want to dismiss the bubble when the user taps anywhere, we need to insert
+        // an invisible button over the background.
+        if dismissTapAnywhere {
+            dismissTarget = UIButton.buttonWithType(UIButtonType.Custom) as UIButton
+            dismissTarget.addTarget(self, action:"dismissTapAnywhereFired:", forControlEvents: UIControlEvents.TouchUpInside)
+            dismissTarget.setTitle("", forState: UIControlState.Normal)
+            dismissTarget.frame = containerView.bounds
+            containerView.addSubview(dismissTarget)
+        }
+        
+        containerView.addSubview(self)
+        
+        // Size of rounded rect
+        var rectWidth = CGFloat(0)
+        let containerViewWidth = containerView.frame.size.width
+        var j:CGFloat!
+        var k:CGFloat!
+        
+        if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+            j = 20
+            k = 1/3
+        } else {
+            j = 10
+            k = 2/3
+        }
+        
+        if maxWidth > 0 {
+            if maxWidth < containerViewWidth {
+                rectWidth = maxWidth
+            } else {
+                rectWidth = containerViewWidth - j
+            }
+        } else {
+            rectWidth = floor(containerViewWidth * k)
+        }
+        
+        var textSize = CGSizeZero
+        
+        if message.isEmpty == false {
+            
+            let textParagraphStyle = NSMutableParagraphStyle()
+            textParagraphStyle.alignment = textAlignment
+            textParagraphStyle.lineBreakMode = NSLineBreakMode.ByWordWrapping
+            
+            textSize = (self.message as NSString).boundingRectWithSize(CGSize(width: rectWidth, height: CGFloat.max), options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: [NSFontAttributeName: textFont, NSParagraphStyleAttributeName: textParagraphStyle], context: nil).size
+        }
+        
+        if let customView = customView {
+            textSize = customView.frame.size
+        }
+        
+        if let title = title {
+            let titleParagraphStyle = NSMutableParagraphStyle()
+            titleParagraphStyle.lineBreakMode = NSLineBreakMode.ByClipping
+            
+            var titleSize = (self.title as NSString).boundingRectWithSize(CGSize(width: rectWidth, height: CGFloat.max), options: NSStringDrawingOptions(rawValue: 0)!, attributes: [NSFontAttributeName: titleFont, NSParagraphStyleAttributeName: titleParagraphStyle], context: nil).size
+            
+            if titleSize.width > textSize.width {
+                textSize.width = titleSize.width
+            }
+            
+            textSize.height += titleSize.height
+        }
+        
+    }
+}
---
+++ Demo/Shared/Demo1ViewController.m
@@ -0,0 +1,202 @@
+//
+//  Demo1ViewController.m
+//  CMPopTipView
+//
+//  Created by Chris Miles on 13/11/10.
+//  Copyright (c) Chris Miles 2010.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//  
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#import "Demo1ViewController.h"
+
+#define foo4random() (1.0 * (arc4random() % ((unsigned)RAND_MAX + 1)) / RAND_MAX)
+
+#pragma mark - Private interface
+
+@interface Demo1ViewController ()
+@property (nonatomic, strong)	NSArray			*colorSchemes;
+@property (nonatomic, strong)	NSDictionary	*contents;
+@property (nonatomic, strong)	id				currentPopTipViewTarget;
+@property (nonatomic, strong)	NSDictionary	*titles;
+@property (nonatomic, strong)	NSMutableArray	*visiblePopTipViews;
+@end
+
+
+#pragma mark - Implementation
+
+@implementation Demo1ViewController
+
+- (void)dismissAllPopTipViews
+{
+	while ([self.visiblePopTipViews count] > 0) {
+		CMPopTipView *popTipView = [self.visiblePopTipViews objectAtIndex:0];
+		[popTipView dismissAnimated:YES];
+		[self.visiblePopTipViews removeObjectAtIndex:0];
+	}
+}
+
+- (IBAction)buttonAction:(id)sender
+{
+	[self dismissAllPopTipViews];
+	
+	if (sender == self.currentPopTipViewTarget) {
+		// Dismiss the popTipView and that is all
+		self.currentPopTipViewTarget = nil;
+	}
+	else {
+		NSString *contentMessage = nil;
+		UIView *contentView = nil;
+		NSNumber *key = [NSNumber numberWithInteger:[(UIView *)sender tag]];
+		id content = [self.contents objectForKey:key];
+		if ([content isKindOfClass:[UIView class]]) {
+			contentView = content;
+		}
+		else if ([content isKindOfClass:[NSString class]]) {
+			contentMessage = content;
+		}
+		else {
+			contentMessage = @"A CMPopTipView can automatically point to any view or bar button item.";
+		}
+		NSArray *colorScheme = [self.colorSchemes objectAtIndex:foo4random()*[self.colorSchemes count]];
+		UIColor *backgroundColor = [colorScheme objectAtIndex:0];
+		UIColor *textColor = [colorScheme objectAtIndex:1];
+		
+		NSString *title = [self.titles objectForKey:key];
+		
+		CMPopTipView *popTipView;
+		if (contentView) {
+			popTipView = [[CMPopTipView alloc] initWithCustomView:contentView];
+		}
+		else if (title) {
+			popTipView = [[CMPopTipView alloc] initWithTitle:title message:contentMessage];
+		}
+		else {
+			popTipView = [[CMPopTipView alloc] initWithMessage:contentMessage];
+		}
+		popTipView.delegate = self;
+		
+		/* Some options to try.
+		 */
+		//popTipView.disableTapToDismiss = YES;
+		//popTipView.preferredPointDirection = PointDirectionUp;
+		//popTipView.hasGradientBackground = NO;
+        //popTipView.cornerRadius = 2.0;
+        //popTipView.sidePadding = 30.0f;
+        //popTipView.topMargin = 20.0f;
+        //popTipView.pointerSize = 50.0f;
+        //popTipView.hasShadow = NO;
+		
+		if (backgroundColor && ![backgroundColor isEqual:[NSNull null]]) {
+			popTipView.backgroundColor = backgroundColor;
+		}
+		if (textColor && ![textColor isEqual:[NSNull null]]) {
+			popTipView.textColor = textColor;
+		}
+        
+        popTipView.animation = arc4random() % 2;
+		popTipView.has3DStyle = (BOOL)(arc4random() % 2);
+		
+		popTipView.dismissTapAnywhere = YES;
+        [popTipView autoDismissAnimated:YES atTimeInterval:3.0];
+
+		if ([sender isKindOfClass:[UIButton class]]) {
+			UIButton *button = (UIButton *)sender;
+			[popTipView presentPointingAtView:button inView:self.view animated:YES];
+		}
+		else {
+			UIBarButtonItem *barButtonItem = (UIBarButtonItem *)sender;
+			[popTipView presentPointingAtBarButtonItem:barButtonItem animated:YES];
+		}
+		
+		[self.visiblePopTipViews addObject:popTipView];
+		self.currentPopTipViewTarget = sender;
+	}
+}
+
+
+#pragma mark - CMPopTipViewDelegate methods
+
+- (void)popTipViewWasDismissedByUser:(CMPopTipView *)popTipView
+{
+	[self.visiblePopTipViews removeObject:popTipView];
+	self.currentPopTipViewTarget = nil;
+}
+
+
+#pragma mark - UIViewController methods
+
+- (void)willAnimateRotationToInterfaceOrientation:(__unused UIInterfaceOrientation)toInterfaceOrientation duration:(__unused NSTimeInterval)duration
+{
+	for (CMPopTipView *popTipView in self.visiblePopTipViews) {
+		id targetObject = popTipView.targetObject;
+		[popTipView dismissAnimated:NO];
+		
+		if ([targetObject isKindOfClass:[UIButton class]]) {
+			UIButton *button = (UIButton *)targetObject;
+			[popTipView presentPointingAtView:button inView:self.view animated:NO];
+		}
+		else {
+			UIBarButtonItem *barButtonItem = (UIBarButtonItem *)targetObject;
+			[popTipView presentPointingAtBarButtonItem:barButtonItem animated:NO];
+		}
+	}
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+	self.visiblePopTipViews = [NSMutableArray array];
+	
+	self.contents = [NSDictionary dictionaryWithObjectsAndKeys:
+					 // Rounded rect buttons
+					 @"A CMPopTipView will automatically position itself within the container view.", [NSNumber numberWithInt:11],
+					 @"A CMPopTipView will automatically orient itself above or below the target view based on the available space.", [NSNumber numberWithInt:12],
+					 @"A CMPopTipView always tries to point at the center of the target view.", [NSNumber numberWithInt:13],
+					 @"A CMPopTipView can point to any UIView subclass.", [NSNumber numberWithInt:14],
+					 @"A CMPopTipView will automatically size itself to fit the text message.", [NSNumber numberWithInt:15],
+					 [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"appicon57.png"]], [NSNumber numberWithInt:16],	// content can be a UIView
+					 // Nav bar buttons
+					 @"This CMPopTipView is pointing at a leftBarButtonItem of a navigationItem.", [NSNumber numberWithInt:21],
+					 @"Two popup animations are provided: slide and pop. Tap other buttons to see them both.", [NSNumber numberWithInt:22],
+					 // Toolbar buttons
+					 @"CMPopTipView will automatically point at buttons either above or below the containing view.", [NSNumber numberWithInt:31],
+					 @"The arrow is automatically positioned to point to the center of the target button.", [NSNumber numberWithInt:32],
+					 @"CMPopTipView knows how to point automatically to UIBarButtonItems in both nav bars and tool bars.", [NSNumber numberWithInt:33],
+					 nil];
+	self.titles = [NSDictionary dictionaryWithObjectsAndKeys:
+				   @"Title", [NSNumber numberWithInt:14],
+				   @"Auto Orientation", [NSNumber numberWithInt:12],
+				   nil];
+	
+	// Array of (backgroundColor, textColor) pairs.
+	// NSNull for either means leave as default.
+	// A color scheme will be picked randomly per CMPopTipView.
+	self.colorSchemes = [NSArray arrayWithObjects:
+						 [NSArray arrayWithObjects:[NSNull null], [NSNull null], nil],
+						 [NSArray arrayWithObjects:[UIColor colorWithRed:134.0/255.0 green:74.0/255.0 blue:110.0/255.0 alpha:1.0], [NSNull null], nil],
+						 [NSArray arrayWithObjects:[UIColor darkGrayColor], [NSNull null], nil],
+						 [NSArray arrayWithObjects:[UIColor lightGrayColor], [UIColor darkTextColor], nil],
+						 [NSArray arrayWithObjects:[UIColor orangeColor], [UIColor blueColor], nil],
+						 [NSArray arrayWithObjects:[UIColor colorWithRed:220.0/255.0 green:0.0/255.0 blue:0.0/255.0 alpha:1.0], [NSNull null], nil],
+						 nil];
+}
+
+@end
