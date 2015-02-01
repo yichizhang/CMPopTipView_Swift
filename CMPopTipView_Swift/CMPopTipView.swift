23e74f6f40bed9c944bba5cca8b0bc3ad6737429
Yichi Zhang <zhang-yi-chi@hotmail.com>
2015-02-01 23:10:43 +1100

    Finished rewriting of "presentPointingAtView:inView:animated:"

--- CMPopTipView_Swift/CMPopTipView.swift
+++ CMPopTipView_Swift/CMPopTipView.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 extension CMPopTipView {
-    func t_presentPointingAtView(targetView:UIView, inView containerView:UIView, animated:Bool) {
+    func t_presentPointingAtView(targetView:UIView, inView containerView:UIView, animated:Bool){
     
         if targetObject == nil {
             targetObject = targetView
@@ -29,7 +29,8 @@ extension CMPopTipView {
         
         // Size of rounded rect
         var rectWidth = CGFloat(0)
-        let containerViewWidth = containerView.frame.size.width
+        let containerViewWidth = containerView.bounds.size.width
+		let containerViewHeight = containerView.bounds.size.height
         var j:CGFloat!
         var k:CGFloat!
         
@@ -92,6 +93,7 @@ extension CMPopTipView {
         var targetRelativeOrigin = targetView.superview!.convertPoint(targetView.frame.origin, toView: superview)
         var containerRelativeOrigin = superview.convertPoint(containerView.frame.origin, toView: superview)
         
+        // FIXME: Debug println
         println("----")
 		println(superview)
 		println(targetView.superview!)
@@ -105,14 +107,111 @@ extension CMPopTipView {
         
         if targetRelativeOrigin.y + targetView.bounds.size.height < containerRelativeOrigin.y {
             
-            pointDirection = PointDirectionUp
-        } else if targetRelativeOrigin.y > containerRelativeOrigin.y + containerView.bounds.size.height {
+            pointDirection = .Up
+        } else if targetRelativeOrigin.y > containerRelativeOrigin.y +  containerViewHeight {
             
-            pointerY = containerView.bounds.size.height
-            pointDirection = PointDirectionDown
+            pointerY =  containerViewHeight
+            pointDirection = .Down
         } else {
             
             pointDirection = preferredPointDirection
+            
+            var targetOriginInContainer = targetView.convertPoint(CGPointZero, toView: containerView)
+            var sizeBelow =  containerViewHeight - targetOriginInContainer.y
+            
+            if pointDirection == .Any {
+                
+                if sizeBelow > targetOriginInContainer.y {
+                    pointDirection = .Up
+                } else {
+                    pointDirection = .Down
+                }
+                
+            }
+            
+            if pointDirection == .Down {
+                pointerY = targetOriginInContainer.y
+            } else {
+                pointerY = targetOriginInContainer.y + targetView.bounds.size.height
+            }
         }
+        
+        let p = targetView.superview!.convertPoint(targetView.center, toView: containerView)
+        var x_p = p.x
+        var x_b = x_p - round(bubbleSize.width * 0.5)
+        
+        if (x_b < sidePadding) {
+            x_b = sidePadding;
+        }
+        if (x_b + bubbleSize.width + sidePadding > containerViewWidth) {
+            x_b = containerViewWidth - bubbleSize.width - sidePadding;
+        }
+        if (x_p - pointerSize < x_b + cornerRadius) {
+            x_p = x_b + cornerRadius + pointerSize;
+        }
+        if (x_p + pointerSize > x_b + bubbleSize.width - cornerRadius) {
+            x_p = x_b + bubbleSize.width - cornerRadius - pointerSize;
+        }
+        
+        let fullHeight = bubbleSize.height + pointerSize + 10
+        var y_b = CGFloat(0)
+        
+        if (pointDirection == .Up) {
+            y_b = topMargin + pointerY;
+            targetPoint = CGPoint(x: x_p-x_b, y: 0);
+        } else {
+            y_b = pointerY - fullHeight;
+            targetPoint = CGPoint(x: x_p-x_b, y: fullHeight-2.0);
+        }
+        
+        var finalFrame = CGRect(
+            x: x_b - sidePadding,
+            y: y_b,
+            width: bubbleSize.width + sidePadding * 2,
+            height: fullHeight
+        )
+        finalFrame = finalFrame.integerRect
+        
+        if animated{
+            if animation == .Slide {
+                alpha = 0
+                var startFrame = finalFrame
+                startFrame.origin.y += 10
+                frame = startFrame
+            } else if animation == .Pop {
+                frame = finalFrame
+                alpha = 0.5
+                
+                // start a little smaller
+                transform = CGAffineTransformMakeScale(0.75, 0.75)
+                
+                // animate to a bigger size
+                UIView.beginAnimations(nil, context: nil)
+                UIView.setAnimationDelegate(self)
+                UIView.setAnimationDidStopSelector("popAnimationDidStop:finished:context:")
+                UIView.setAnimationDuration(0.15)
+                
+                transform = CGAffineTransformMakeScale(1.1, 1.1)
+                alpha = 1.0
+                
+                UIView.commitAnimations()
+            }
+            
+            setNeedsDisplay()
+            
+            if animation == .Slide {
+                UIView.beginAnimations(nil, context: nil)
+                
+                alpha = 1.0
+                frame = finalFrame
+                
+                UIView.commitAnimations()
+            }
+        } else {
+            
+            setNeedsDisplay()
+            frame = finalFrame
+        }
+        
     }
 }
